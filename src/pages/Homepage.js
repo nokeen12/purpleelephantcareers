97c27c188478ffe5f1ba5297f311833c0bfbb6af
Cordre <cordrepayne@gmail.com>
2022-08-24 16:28:04 -0500

    created header

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,22 @@
 function Homepage(){
     return(
-        <div>
-            homepage
+        <div className="homepage">
+            <header className="section one">
+                <div className="header left">
+                    <h1>Helping You Address The Elephant In Your Career Journey</h1>
+                    <p>As a Career Coach and resume writer,  my goal is to help you recognize and show how good you actually are! </p>
+                    <button>Book An Appointment</button>
+                </div>
+                <div className="header right">
+                    <div className="square">
+                    </div>
+                    <img src="headshot.png" alt="kelli carroll" id="kelli"/>
+                    <button>Kelli M. Carroll, MS Ed., CCSP</button>
+                </div>
+            </header>
+            <div classNAme="section two">
+                section 2
+            </div>
         </div>
     )
 }
