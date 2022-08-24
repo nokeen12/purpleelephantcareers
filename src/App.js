c3cbcc3a53ba0cebba8ada07d1ed287112cf84ba
Cordre <cordrepayne@gmail.com>
2022-08-23 23:47:26 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <div className='routes'>
+        <Routes>
+          <Route path='/' element={ <Homepage /> }/>
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './Navbar.css';
+// import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import { Link } from "react-router-dom";
+function Navbar(){
+    return(
+        <div className='navbar'>
+            <div className="leftSide">
+                <Link to="/">Logo | Purple Elephant Careers</Link>
+            </div>
+            <div className="rightSide">
+                <Link to="/home">Home</Link>
+                <Link to="/about">About us</Link>
+                <Link to="/services">Services</Link>
+                <Link to="/book">Book an Appointment</Link>
+                {/* <HamburgerMenu /> */}
+            </div>
+        </div>
+    )
+}
+export default Navbar;
