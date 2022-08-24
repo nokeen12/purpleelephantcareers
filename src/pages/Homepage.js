8e424fb118098d9a01e32909b9c7c580e024828a
Cordre <cordrepayne@gmail.com>
2022-08-24 16:46:21 -0500

    create section two outline

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -14,8 +14,30 @@ function Homepage(){
                     <button>Kelli M. Carroll, MS Ed., CCSP</button>
                 </div>
             </header>
-            <div classNAme="section two">
-                section 2
+            <div className="section two">
+                <h2>Kelli is Genuine, Knowledgeable, Efficient, Timely and Honest.</h2>
+                <ul>
+                    <li>Goes above and beyond with her work</li>
+                    <li>Works to build genuine relationships with her clients </li>
+                    <li>Provides access to her extensive network and resources</li>
+                </ul>
+                <h2>Testimonials</h2>
+                <div className="testimonials">
+                    <div className="block">
+                        <div>icon</div>
+                        <div>text</div>
+                        <div>name</div>
+                        <div>see more link</div>
+                    </div>
+                    <div className="block">
+                        <div>icon</div>
+                        <div>text</div>
+                        <div>name</div>
+                        <div>see more link</div>
+                    </div>
+                </div>
+                <h2>Still hesitating? Schedule a free consultation!</h2>
+                <button>Book An Appointment</button>
             </div>
         </div>
     )
