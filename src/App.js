fff1a48e6da229ec9f1080f96a7f1b68c8f51111
Cordre <cordrepayne@gmail.com>
2022-08-24 18:15:49 -0500

    create footer and testimonial boxes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Homepage from './pages/Homepage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path='/' element={ <Homepage /> }/>
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+function Footer(){
+    return(
+        <footer>
+            <p>Copyright Purple Elephant Careers LLC</p>
+            <div className="creators">
+                <p>Design by Ironhack Dream Team, 2022</p>
+                <p>Website Developed by Cordre Payne</p>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
