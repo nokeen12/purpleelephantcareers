d03b8fa8e1ce3f0b8ba454f3e1ca3e16c8ff5f8f
Nokeen <cordrepayne@gmail.com>
2022-08-27 22:27:31 -0500

    Add service page content and setup

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+
 function Services(){
     return(
-        <div>
-            services
+        <div id="services">
+            <h2>Services</h2>
+            <div id="serviceSection">
+                <div className="serviceBlock">
+                    <img src="empty" alt="empty" />
+                    <div className="serviceHover">Writing</div>
+                    <ul>
+                        <li>Resumes</li>
+                        <li>Cover Letter</li>
+                        <li>LinkedIn</li>
+                        <li>Bio</li>
+                        <li>Thank you Notes</li>
+                    </ul>
+                </div>
+                <div className="serviceBlock">
+                    <img src="empty" alt="empty" />
+                    <div className="serviceHover">Coaching</div>
+                    <ul>
+                        <li>Job Search</li>
+                        <li>Exploration / Discovery</li>
+                        <li>Transition</li>
+                        <li>Negotiation</li>
+                        <li>Interviewing</li>
+                    </ul>
+                </div>
+            </div>
+            <p>*Packages Can Be Made With Any Combination Of Services, Based On Individual Needs.</p>
+            <h2>Why Hesitate? Schedule A Free Consultation Now!</h2>
+            <Link to="/booking" className="button three">BOOK AN APPOINTMENT</Link>
         </div>
     )
 }
