f9157e4acd1e75643918c8248614ebf253dddeca
Nokeen <cordrepayne@gmail.com>
2022-08-28 19:43:39 -0500

    Embed calendly on booking page

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+
 function Booking(){
+    useEffect(() => {
+        const script = document.createElement('script');
+      
+        script.src = "https://assets.calendly.com/assets/external/widget.js";
+        script.async = true;
+      
+        document.body.appendChild(script);
+      
+        return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
     return(
         <div>
             booking page
+            {/* <!-- Calendly inline widget begin --> */}
+            <div className="calendly-inline-widget" data-url="https://calendly.com/kellimcarroll?hide_landing_page_details=1"></div>
+            {/* <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script> */}
+            {/* <!-- Calendly inline widget end --> */}
         </div>
     )
 }
