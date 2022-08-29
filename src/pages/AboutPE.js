d2beb1d2ef8fb3cf9bdda6f2a60d7a9abf218ce8
Nokeen <cordrepayne@gmail.com>
2022-08-28 21:16:43 -0500

    Add content to aboutpe page

--- src/pages/AboutPE.js
+++ src/pages/AboutPE.js
@@ -1,7 +1,25 @@
 function AboutPE(){
     return(
-        <div>
-            about purple elephant
+        <div id="aboutPE">
+            <h2>About Purple Elephant</h2>
+            <div>
+                <img src="blank" alt="blank"/>
+                <p>
+                Purple Elephant Careers, LLC. was created in April 2022 by Kelli M. Carroll. The idea for the company came to her while at the 2022 Career Thought Leaders Symposium in Philadelphia, PA. Feeling inspired and encouraged by her Career Thought Leader Colleagues, Kelli decided to take the plunge and start her own company.
+                </p>
+            </div>
+            <div>
+                <img src="blank" alt="blank"/>
+                <p>
+                Purple Elephant Careers was born out of a desire to support the “Ennial” generations - Xennials, Millennials, and Zillennials – specifically of color. As a Millennial, Kelli noticed the lack of career support from others for her generation, as well as the constant bashing of the “ennials” on the internet.
+                </p>
+            </div>
+            <div>
+                <img src="blank" alt="blank"/>
+                <p>
+                The name Purple Elephant comes from Kelli’s love for purple and elephants. Purple is the color of royalty and is known to uplift, enlighten, inspire, and encourage. Elephant’s symbolize strength, good fortune, and happiness. They are sturdy animals known for their fortitude on long journeys. The career journey is one that is long and requires strength, fortitude, encouragement, and more. However, when you reach the peak, you receive good fortune, happiness, and your spirit is uplifted, enlightened and inspired.
+                </p>
+            </div>
         </div>
     )
 }
